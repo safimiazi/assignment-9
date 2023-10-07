5030929b08abdc087cc8b764accfc1b009719f1f
safimiazi <mohibullamiazi@gmail.com>
2023-10-07 15:46:58 +0600

    login and register page create and design done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,14 +1,21 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
+  const handleForm = e => {
+    e.preventDefault()
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password)
+}
   return (
 
     <div className="hero min-h-screen">
       <div className="p-10 bg-orange-500 rounded-xl   hero-content flex-col lg:flex-row-reverse">
 
         <div className="bg-orange-200 card flex-shrink-0 w-full max-w-sm shadow-2xl">
-          <form className="card-body">
-            <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+          <form onSubmit={handleForm} className="card-body">
+            <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -27,6 +34,7 @@ const Login = () => {
             <div className="form-control mt-6">
               <button className="btn bg-orange-500 hover:bg-orange-600 text-white">Login</button>
             </div>
+            <p>New user? please <Link className="text text-orange-800 underline" to="/register">register</Link></p>
           </form>
         </div>
       </div>
--- src/Pages/Ragister/Register.jsx
+++ src/Pages/Ragister/Register.jsx
@@ -1,8 +1,48 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
+    const handleForm = e => {
+        e.preventDefault()
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password)
+    }
     return (
-        <div>
-            
+        <div className="hero min-h-screen">
+            <div className="p-10 bg-orange-500 rounded-xl   hero-content flex-col lg:flex-row-reverse">
+
+                <div className="bg-orange-200 card flex-shrink-0 w-full max-w-sm shadow-2xl">
+                    <form onSubmit={handleForm} className="card-body">
+                        <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn bg-orange-500 hover:bg-orange-600 text-white">Login</button>
+                        </div>
+                        <p>Already register? please <Link className="text text-orange-800 underline" to="/login">Login</Link></p>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
